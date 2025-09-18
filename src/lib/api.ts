b8b84180afe2c017a13df2c83c29a3f2b21c6550
Goddy-2024 <gaoajuoga@gmail.com>
2025-09-18 09:26:35 +0300

    revert changes check latter

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,4 @@
-const APIURL = 'http://localhost:5000/api';
-const API_BASE_URL = APIURL || 'https://api.dawacoffeemachienesandaccessories.com/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://api.dawacoffeemachienesandaccessories.com/api';
 
 class ApiClient {
   private baseURL: string;
